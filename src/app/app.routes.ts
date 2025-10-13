40dbdcb77fec81a225563ab00680004fe22f90ab
MarkoBrie <marko.briesemann@gmail.com>
2025-10-13 16:06:54 +0200

    final commit chapter 10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { ActivityPage } from './pages/activity-page/activity-page';
 import { ActivityViewer } from './pages/activity-page/activity-viewer/activity-viewer';
 import { ActivityEditor } from './pages/activity-page/activity-editor/activity-editor';
+import { LandingPage } from './pages/landing-page/landing-page';
 
 export const routes: Routes = [
+    { path: '', component: LandingPage, pathMatch: 'full' },
     { path: '', component: ActivityPage, pathMatch: 'full' },
     { path: 'activities', component: ActivityPage, pathMatch: 'full' },
     { path: 'activity/:id', component: ActivityViewer, pathMatch: 'full' },
     { path: 'activity-editor', component: ActivityEditor, pathMatch: 'full' },
     { path: 'activity-editor/:id', component: ActivityEditor, pathMatch: 'full' },
+    
 ];
